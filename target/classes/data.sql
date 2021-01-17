5ce04dc63b753cea34773e372409a7624094a9a0
Shahnavaz Saiyad <ssaiyed173@gmail.com>
2021-01-17 13:35:14 +0530

    Sql data

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,2 +1,7 @@
-insert into CREDIT_PERIOD (ID, GV, SC ) values (1, 60, 30);
-insert into BALANCE_SHEET (ID, GV, SC ) values (1, 50, 1002); 
+CREATE TABLE credit_period(id int PRIMARY KEY, gv int, sc int);
+CREATE TABLE balance_sheet(id int PRIMARY KEY, gv int, sc int);
+CREATE TABLE price(id int PRIMARY KEY, show VARCHAR(30), winningDate DATE, creditDate DATE, credited boolean, type VARCHAR(10), amount int);
+CREATE TABLE show(id int PRIMARY KEY, name VARCHAR(30), active VARCHAR(10));
+
+insert into credit_period (id, gv, sc ) values (1, 60, 30);
+insert into balance_sheet (id, gv, sc ) values (1, 50, 1002); 
